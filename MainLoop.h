2caa884d8e7e7398c61344e8b09cdc06779ca9c5
Sherrill374 <lebshe2207@students.ecpi.edu>
2026-04-02 15:37:03 -0400

    Add includes for write, read, and calculation files
    Added includes for file operations and calculations.

--- MainLoop.h
+++ MainLoop.h
@@ -1,6 +1,11 @@
 #ifndef MAINLOOP_H
 #define MAINLOOP_H
 
+#include "writeFile.h"
+#include "readFile.h"
+#include "firstCalculation.h"
+#include "secondCalculation.h"
+
 void mainLoop(int option);
 
 #endif
